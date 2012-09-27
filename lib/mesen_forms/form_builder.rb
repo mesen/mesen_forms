bad99b2462a8ff5fc0072559cfa7c0ef6f6e6c0b
Geir Arne Brevik <geirarne@mesen.no>
2012-09-27 22:17:56 +0200

    THIS IS IT ..................................................................................... i think

--- lib/mesen_forms/form_builder.rb
+++ lib/mesen_forms/form_builder.rb
@@ -58,13 +58,13 @@ module MesenForms
         label(attribute, class: 'control-label')+
         controls do
           content_tag :div, :class => 'well fields-wrapper' do
-            # fields_for attribute do |field|
-            #   @template.render(attribute.to_s.singularize + '_fields', :f => field)
-            # end
-            content_tag :div, :class => ('hidden' if object.instance_eval(attribute).any?)
+            fields_for attribute do |field|
+              render(attribute.to_s.singularize + '_fields', :f => field)
+            end+
+            content_tag :div, :class => ('hidden' if object.instance_eval(attribute).any?) do
               'no_' + I18n.t(object.class.to_s.underscore, :scope => [:activerecord, :models])
             end
-          end
+          end+
           link_to_add_fields(I18n.t('add_' + attribute.to_s.singularize, :scope => [:layouts, :admin]), self, attribute)
         end
       end
