bb0caf21a19d766f548dede7e87279b139844506
Geir Arne Brevik <geirarne@mesen.no>
2012-09-27 20:33:00 +0200

    trying without plus char

--- lib/mesen_forms/form_builder.rb
+++ lib/mesen_forms/form_builder.rb
@@ -59,12 +59,12 @@ module MesenForms
         controls do
           content_tag :div, :class => 'well fields-wrapper' do
             fields_for attribute do |field|
-              @template.render attribute.to_s.singularize + '_fields', :f => field
-            end+
+              @template.render(attribute.to_s.singularize + '_fields', :f => field)
+            end
             content_tag :div, :class => ('hidden' if object.instance_eval(attribute).any?)
               'no_' + I18n.t(object.class.to_s.underscore, :scope => [:activerecord, :models])
             end
-          end+
+          end
           @template.link_to_add_fields(I18n.t('add_' + attribute.to_s.singularize, :scope => [:layouts, :admin]), self, attribute)
         end
       end
