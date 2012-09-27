c5ede95f9a1ae1b9e9f5a309156525c723b7fcb0
Geir Arne Brevik <geirarne@mesen.no>
2012-09-27 14:28:35 +0200

    improved form builder for nested forms

--- lib/mesen_forms/form_builder.rb
+++ lib/mesen_forms/form_builder.rb
@@ -60,9 +60,9 @@ module MesenForms
           content_tag :div, :class => 'well fields-wrapper' do
             self.fields_for attribute do |field|
               @template.render attribute.singularize << '_fields', :f => field
-            end+
+            end
             content_tag :div, :class => ('hidden' if object.instance_eval(attribute).any?)
-              I18n.t(object.class.to_s.underscore, :scope => [:activerecord, :models]
+              I18n.t(object.class.to_s.underscore, :scope => [:activerecord, :models])
             end
           end+
           @template.link_to_add_fields(I18n.t('Add ' + attribute.singularize, :scope => [:layouts, :admin]), self, attribute)
