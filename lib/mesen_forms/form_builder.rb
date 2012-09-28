f86b45e732fe77904387dbbe7438f1126a260362
Geir Arne Brevik <geirarne@mesen.no>
2012-09-28 08:51:22 +0200

    GOOD MORNING .. or something like that

--- lib/mesen_forms/form_builder.rb
+++ lib/mesen_forms/form_builder.rb
@@ -58,8 +58,10 @@ module MesenForms
         label(attribute, class: 'control-label')+
         controls do
           content_tag(:div, :class => 'well fields-wrapper') do
-            fields_for(object, attribute) do |field|
-              render(attribute.to_s.singularize + '_fields', :f => field)
+            if object.instance_eval(attribute.to_s).any?
+              fields_for(object.instance_eval(attribute).to_s) do |field|
+                render(attribute.to_s.singularize + '_fields', :f => field)
+              end
             end+
             content_tag(:div, I18n.t("no_" + object.class.name.to_s.downcase, :scope => [:layouts, :admin]).html_safe, class: ('hidden' if object.instance_eval(attribute.to_s).any?))
           end+
