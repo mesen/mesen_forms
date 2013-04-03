0de543ec523f2054c8cae26d8b3563b409b90966
Kathrin Pilka <kathrinpilka@Kathrins-MacBook-Pro-2.local>
2013-04-03 21:42:07 +0200

    test

--- lib/mesen_forms/helpers/form_helper.rb
+++ lib/mesen_forms/helpers/form_helper.rb
@@ -109,11 +109,15 @@ module MesenForms
         end.join(" ").html_safe
       end
   
-      def link_to_add_fields(name, f, association)
+      def link_to_add_fields(name, f, association, renderform)
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.fields_for(association, new_object, child_index: id) do |builder|
-          render(association.to_s.singularize + "_fields", f: builder)
+          if !renderform.nil?
+            render(renderform, f: builder)
+          else
+            render(association.to_s.singularize + "_fields", f: builder)
+          end
         end
         link_to(name, '#', class: "add_fields btn", data: {id: id, fields: fields.gsub("\n", "")})
       end
